53a28bd3d2220cb62578d8b1633e6aab2571fb94
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-06-28 18:11:29 +0300

    configure

--- LogifyAlertClient.php
+++ LogifyAlertClient.php
@@ -3,17 +3,28 @@ require_once('/Collectors/Report.php');
 require_once('/ReportSender.php');
 
 class LogifyAlertClient {
+	public $apiKey;
+	public $serviceUrl;
 	//$attachments ='';
 	//$customData ='';
 	//$instance ='';
-	public $userId ='';
+	public $userId;
 
 	function send(Exception $exception){
-		$sender = new ReportSender();
+		$this->configure();
+		$sender = new ReportSender($this->apiKey, $this->serviceUrl);
 		$report = new ReportCollector($exception);
-		//echo '<pre>'.print_r($GLOBALS, 1).'</pre>';
-		//echo '<pre>'.print_r($report->GetData(),1).'</pre>';
 		return $sender->send( $report->CollectData() );
+	}
+	function configure() {
+		include('/config.php');
+		$configs = new LogifyAlert();
+		if(empty($this->apiKey)){
+			$this->apiKey = $configs::apiKey;
+		}
+		if(empty($this->serviceUrl)){
+			$this->serviceUrl = $configs::serviceUrl;
+		}
 	}
 }
 ?>
--- ReportSender.php
+++ ReportSender.php
@@ -1,13 +1,19 @@
 <?php
 class ReportSender{
+	public $apiKey;
+	public $serviceUrl;
+
+	function __construct($apiKey, $serviceUrl){
+		$this->apiKey = $apiKey;
+		$this->serviceUrl = $serviceUrl;
+	}
+
     function send( $data ) {
-		include('/config.php');
-		$configs = new LogifyAlert();
 		$json = json_encode( $data );
-		$header = $this->generate_header(strlen($json), $configs::apiKey);
+		$header = $this->generate_header( strlen($json) );
 		$request = curl_init();
 		curl_setopt_array( $request, [
-		    CURLOPT_URL => $configs::serviceUrl,
+		    CURLOPT_URL => $this->serviceUrl,
 		    CURLOPT_RETURNTRANSFER => true,
 		    CURLOPT_HTTPHEADER => $header,
 		    CURLOPT_POST => true,
@@ -23,9 +29,9 @@ class ReportSender{
 		}
 		return $response;
     }
-    function generate_header( $content_length, $apiKey ) {
+    function generate_header( $content_length ) {
         $header = array(
-            'Authorization: amx '. $apiKey,
+            'Authorization: amx '. $this->apiKey,
             'Content-Type: application/json',
             'Content-Length: '.$content_length,
         );
