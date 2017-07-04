bb3b83ca6edb58171c2330e97fe109ee87b3f132
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-07-04 12:05:58 +0300

    attachment

--- Core/Attachment.php
+++ Core/Attachment.php
@@ -3,5 +3,16 @@ class Attachment {
     public $name;
     public $mimeType;
     public $content;
+
+    function GetAttachmentData(){
+        $result = array();
+        $result['name'] = $this->name;
+        $result['mimeType'] = $this->mimeType;
+        $result['content'] = $this->GetEncodedContent();
+        $result['compress'] = 'gzip';
+    }
+    private function GetEncodedContent(){
+        
+    }
 }
 ?>
