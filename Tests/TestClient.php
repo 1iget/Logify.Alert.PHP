8da844aff3ce47ebab6ca7123ecae9750ff9077e
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-07-04 11:52:08 +0300

    refactoring tests

--- Tests/TestClient.php
+++ Tests/TestClient.php
@@ -1,6 +1,6 @@
 <?php
 require_once(__DIR__.'/clientForTest.php');
-class ClientTest extends PHPUnit_Framework_TestCase {
+class ConfigTest extends PHPUnit_Framework_TestCase {
     private $client;
 
     protected function setUp(){
