dd525f57cb4c826900e8ffe8585896d816eaad88
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-07-06 17:56:37 +0300

    config for debug

--- Config.php
+++ Config.php
@@ -1,10 +1,11 @@
 <?php
 class LogifyAlert{
-	const serviceUrl = 'http://logify.devexpress.com/api/report/newreport';
+	//const serviceUrl = 'http://logify.devexpress.com/api/report/newreport';
+    const serviceUrl = 'http://logify.devexpress.devx:59240/api/report/newreport';
 	const apiKey = '2F7B18D129F940E0A512956BF4AB9561';
-    const userId = 'php test user';
+    const userId = 'php user';
     const appName = 'Test PHP Application';
-    const appVersion = '1.0.0.0';
+    const appVersion = '2.1.1.1';
 
     public $globalVariablesPermissions = array(
         'get' => true,
