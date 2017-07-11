a850679c68b5c7ccd889cd67aeb81f28803c8b2e
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-07-11 16:00:47 +0300

    load helper

--- Logify/LoadHelper.php
+++ Logify/LoadHelper.php
@@ -3,22 +3,54 @@ namespace DevExpress;
 
 class LoadHelper {
     public static function LoadModule($className) {
-        if(strpos($className, 'iCollector') || strpos($className, 'iVariables') ){
-            $filePath =__DIR__.'/Core/Interfaces.php';
-            require_once ($filePath);
-            return;
-        }
-        $namespaceName = 'DevExpress\\Logify';
-        //$namespaceName = __NAMESPACE__;
+        $namespaceName = 'DevExpress\\Logify\\';
         $namespaceNamePos = strpos($className, $namespaceName);
         if ($namespaceNamePos === 0) {
             $subFolderPath = substr($className, $namespaceNamePos + strlen($namespaceName));
-            $filePath = __DIR__.str_replace("\\", DIRECTORY_SEPARATOR, $subFolderPath).".php";
-            if(strpos($filePath, 'Collectors')){
-                $filePath = str_replace('Collector.php', '.php', $filePath);
-            }
+            $classPath = explode("\\", $subFolderPath);
+            $filePath = __DIR__.self::getFileName($classPath);
             require_once($filePath);
         }
     }
+
+    static function getFileName($classPathArray){
+        $result = DIRECTORY_SEPARATOR;
+        $currentArray = self::files;
+        foreach($classPathArray as $currentName ){
+            if(is_array($currentArray[$currentName])){
+                $result = $result.$currentName.DIRECTORY_SEPARATOR;
+                $currentArray = $currentArray[$currentName];
+            }else{
+                $result = $result.$currentArray[$currentName];
+            }
+        }
+        return $result;
+    }
+
+    const files = array(
+                'Collectors' => array(
+                    'AttachmentsCollector' => 'Attachments.php',
+                    'CustomDataCollector' => 'CustomData.php',
+                    'DateTimeCollector' => 'DateTime.php',
+                    'DevPlatformCollector' => 'DevPlatform.php',
+                    'ExceptionCollector' => 'Exception.php',
+                    'ExtensionsCollector' => 'Extensions.php',
+                    'GlobalVariablesCollector' => 'GlobalVariables.php',
+                    'LogifyAppCollector' => 'LogifyApp.php',
+                    'MemoryCollector' => 'Memory.php',
+                    'OSCollector' => 'OS.php',
+                    'PlatformCollector' => 'Platform.php',
+                    'ProtocolVersionCollector' => 'ProtocolVersion.php',
+                    'ReportCollector' => 'Report.php',
+                    'VariablesCollector' => 'Variables.php',
+                ),
+                'Core' => array(
+                    'Attachment' => 'Attachment.php',
+                    'iCollector' => 'Interfaces.php',
+                    'iVariables' => 'Interfaces.php',
+                    'ReportSender' => 'ReportSender.php',
+                ),
+                'LogifyAlertClient' => 'LogifyAlertClient.php',
+    );
 }
 ?>
