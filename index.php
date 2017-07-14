95b15dd425c3dc3090586b1d4c4d788a5efc46f4
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-07-14 17:06:13 +0300

    fix names for docs

--- index.php
+++ index.php
@@ -21,25 +21,25 @@ if(array_key_exists ( 'throwButton' , $_POST )){
     //$client->customData = $customData;
 
     $client->start_exceptions_handling();
-    $client->set_can_report_exception_callback('can_ReportException');
-    $client->set_before_report_exception_callback('before_ReportException');
-    $client->set_after_report_exception_callback('after_ReportException');
+    $client->set_can_report_exception_callback('can_report_exception');
+    $client->set_before_report_exception_callback('before_report_exception');
+    $client->set_after_report_exception_callback('after_report_exception');
     throwMyEx();
 }
 function throwMyEx(){
     strpos(); //throw
-    strpos2(); //errors
+    //strpos2(); //errors
     throw new Exception('test PHP Exception');
 }
-function can_ReportException($exception){
+function can_report_exception($exception){
     return true;
 }
-function before_ReportException(){
+function before_report_exception(){
     $client = LogifyAlertClient::get_instance();
     $client->customData = get_CustomData();
     $client->attachments = get_Attachments();
 }
-function after_ReportException($response){
+function after_report_exception($response){
     if($response !== true){
         echo $response.'<br />';
     }
