1ff9a27d830916e49b1874f7cbce8667e0008a49
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-07-24 18:23:02 +0300

    Autoloader refactoring for composer

--- Logify/LoadHelper.php
+++ Logify/LoadHelper.php
@@ -7,53 +7,10 @@ class LoadHelper {
         $namespaceNamePos = strpos($className, $namespaceName);
         if ($namespaceNamePos === 0) {
             $subFolderPath = substr($className, $namespaceNamePos + strlen($namespaceName));
-            $classPath = explode("\\", $subFolderPath);
-            $filePath = __DIR__.self::getFileName($classPath);
+            $classPath = $subFolderPath.'.php';
+            $filePath = __DIR__.DIRECTORY_SEPARATOR.$classPath;
             require_once($filePath);
         }
-    }
-
-    static function getFileName($classPathArray){
-        $result = DIRECTORY_SEPARATOR;
-        $currentArray = self::getFilesArray();
-        foreach($classPathArray as $currentName ){
-            if(is_array($currentArray[$currentName])){
-                $result = $result.$currentName.DIRECTORY_SEPARATOR;
-                $currentArray = $currentArray[$currentName];
-            }else{
-                $result = $result.$currentArray[$currentName];
-            }
-        }
-        return $result;
-    }
-    static function getFilesArray(){
-        return array(
-                'Collectors' => array(
-                    'AttachmentsCollector' => 'Attachments.php',
-                    'CustomDataCollector' => 'CustomData.php',
-                    'DateTimeCollector' => 'DateTime.php',
-                    'DevPlatformCollector' => 'DevPlatform.php',
-                    'ExceptionCollector' => 'Exception.php',
-                    'ExtensionsCollector' => 'Extensions.php',
-                    'GlobalVariablesCollector' => 'GlobalVariables.php',
-                    'LogifyAppCollector' => 'LogifyApp.php',
-                    'MemoryCollector' => 'Memory.php',
-                    'OSCollector' => 'OS.php',
-                    'PlatformCollector' => 'Platform.php',
-                    'ProtocolVersionCollector' => 'ProtocolVersion.php',
-                    'ReportCollector' => 'Report.php',
-                    'VariablesCollector' => 'Variables.php',
-                    'UserAgentCollector' => 'UserAgent.php',
-                    'PHPVersionCollector' => 'PHPVersion.php',
-                ),
-                'Core' => array(
-                    'Attachment' => 'Attachment.php',
-                    'iCollector' => 'Interfaces.php',
-                    'iVariables' => 'Interfaces.php',
-                    'ReportSender' => 'ReportSender.php',
-                ),
-                'LogifyAlertClient' => 'LogifyAlertClient.php',
-    );
     }
 }
 ?>
