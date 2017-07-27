e9f90f55b61f557b465a6b9a16b7335f9ca0befa
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-07-27 16:14:20 +0300

    fix testing bug

--- Config.php
+++ Config.php
@@ -7,7 +7,7 @@ class LogifyAlert{
         'appName' => 'PHP Application 1.2',
         'appVersion' => '1.2.0.0',
         );
-    public $collectExtensions = true;
+    public $collectExtensions = false;
     public $offlineReportsCount = 10;
     public $offlineReportsDirectory = 'C:/Temp/lartrprt/';
     public $offlineReportsEnabled = true;
@@ -19,7 +19,7 @@ class LogifyAlert{
         'files' => false,
         'environment' => false,
         'request' => false,
-        'server' => true,
+        'server' => false,
         );
 }
 ?>
