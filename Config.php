fe0064fd0684565ae6828d54424f0a9978f7d0d4
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-11-23 14:22:11 +0300

    publish config

--- Config.php
+++ Config.php
@@ -2,11 +2,10 @@
 class LogifyAlert {
 
     public $settings = array(
-        //'serviceUrl' => 'http://logify.devexpress.devx:59240/api/report/newreport',
         'apiKey' => '2F7B18D129F940E0A512956BF4AB9561',
-        'userId' => 'php test user 1.2',
-        'appName' => 'PHP Application 1.2',
-        'appVersion' => '1.2.0.0',
+        'userId' => 'php user',
+        'appName' => 'Test PHP Application',
+        'appVersion' => '0.0.0.1',
     );
     public $collectExtensions = false;
     
